905d2b5eed7163adfa112a0068c7d0870154adca
SAZHU1228 <71476517+SAZHU1228@users.noreply.github.com>
2021-04-23 11:53:48 +0800

    期中

---
+++ 2DPixelGame/Assets/MainCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    public float speed = 1.5f;
+    public Vector2 limitY = new Vector2(-5, 5);
+    public Vector2 limitX = new Vector2(-5, 5);
+    public Transform player;
+
+    private void Update()
+    {
+        Track();
+    }
+
+    private void Track()
+    {
+        Vector3 posCam = transform.position;
+        Vector3 posPla = player.position;
+
+        posCam = Vector3.Lerp(posCam, posPla, 0.5f * speed * Time.deltaTime);
+        posCam.z = -10;
+
+        posCam.x = Mathf.Clamp(posCam.x, limitX.x, limitX.y);
+        posCam.y = Mathf.Clamp(posCam.y, limitY.x, limitY.y);
+
+        transform.position = posCam;
+    }
+}
---
+++ 2DPixelGame/Assets/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+[Header("移動速度"),Range(0,300)]
+public float speed = 10.5f;
+[Header("虛擬搖桿")]
+public FixedJoystick joystick;
+[Header("變形元件")]
+public Transform tra;
+[Header("動畫元件")]
+public Animator ani;
+[Header("偵測範圍")]
+public float rangeAttack = 2.5f;
+
+    // 方法
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(1, 0, 0, 0.2f);
+        Gizmos.DrawSphere(transform.position, rangeAttack);
+    }
+    private void Attack()
+{
+        print("攻擊");
+
+        RaycastHit2D hit = Physics2D.CircleCast(transform.position, rangeAttack, transform.right, 0, 1 << 8);
+        print("碰到的物件:" + hit.collider.name);
+
+        if (hit && hit.collider.tag == "道具") Destroy(hit.collider.gameObject);
+}
+private void Move()
+{
+        float h = joystick.Horizontal;
+        float v = joystick.Vertical;
+
+        tra.Translate(h * speed * Time.deltaTime, v * speed * Time.deltaTime, 0);
+        ani.SetFloat("水平", h);
+}
+ //語法
+    private void Start()
+    {
+        Move();
+    }
+    private void Update()
+    {
+        Move();
+    }
+}
